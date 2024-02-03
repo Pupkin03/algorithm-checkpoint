966a26436d487817778f686768dae1e085c72f0a
Abejide Ayomide <Abejideyoungzee@gmail.com>
2024-02-03 16:06:44 +0100

    latest

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var len = txt.length;
 document.getElementById("demo").innerHTML = len;
+
+
+
+
+function countWords(sentence) {
+  
+    let count = 1;
+ 
+    for (let char of sentence) {
+        if (char === ' ') {
+            count++;
+        }
+    }
+ 
+    return count;
+}
+
+let sentence = "Programming is fun and challenging";
+console.log("Number of words:", countWords(sentence));
+
+
+
+
+
+
+
+
+    
+      function isVowel(ch) { 
+        ch = ch.toUpperCase(); 
+        return ch == "A" || ch == "E" || ch == "I" || ch == "O" || ch == "U"; 
+      } 
+
+      function countVowels(str) 
+      { 
+        var count = 0; 
+        for (var i = 0; i < str.length; i++) 
+          if (isVowel(str[i])) 
+            
+            ++count; 
+        return count; 
+      } 
+  
+
+      var str = "abc de"; 
+  
+
+      document.write(countVowels(str)); 
+      document.write("<br>"); 
+        
